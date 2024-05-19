6929454b273d2214ff37b1bd2d92e9d31eb4c18d
Aashna-Verma <aashna.verma@outlook.com>
2024-05-19 16:29:52 -0400

    trying out models

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import mongoose from "mongoose";
 
-const app = express();
+import('dotenv').then(dotenv => {
+  dotenv.config();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+  const app = express();
+
+  app.get("/hello", (_, res) => {
+    res.send("Hello Vite + React + TypeScript!");
+  });
+
+  mongoose.connect("mongodb+srv://admin:"+ process.env.MONGODB_PASSWORD +"@planitdb.7j7rlyn.mongodb.net/?retryWrites=true&w=majority&appName=PlanItDB")
+    .then(() => {
+      console.log('Connected to MongoDB')
+      
+      ViteExpress.listen(app, 3000, () =>
+        console.log("Server is listening on port 3000..."),
+      );
+      
+    })
+    .catch(err => console.error('Failed to connect to MongoDB', err));
 });
-
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
-);
